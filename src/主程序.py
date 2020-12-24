613c33504b58a75d46a59caca8f8f92eb967a854
Anthony Hoo <tonyhoo2000@hotmail.com>
2020-12-24 20:23:27 +0800

    一个简单实现

---
+++ src/主程序.py
@@ -0,0 +1,10 @@
+import 查电费
+import 生成rss
+import time
+
+if __name__ == "__main__":
+    rooms = ["B101", "B102", "B219"]
+    for roomID in rooms:
+        电费余额 = 查电费.获取电费json(roomID)
+        生成rss.生成rss文件(roomID, 电费余额)
+        time.sleep(0.5)
