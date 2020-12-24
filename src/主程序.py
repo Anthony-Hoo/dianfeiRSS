b5323e85c389ac500fbf783546f0953ad06d8233
Anthony Hoo <tonyhoo2000@hotmail.com>
2020-12-24 21:25:59 +0800

    增加轮询等待，并修正xml文件存储位置

--- src/主程序.py
+++ src/主程序.py
@@ -4,7 +4,16 @@ import time
 
 if __name__ == "__main__":
     rooms = ["B101", "B102", "B219"]
-    for roomID in rooms:
-        电费余额 = 查电费.获取电费json(roomID)
-        生成rss.生成rss文件(roomID, 电费余额)
-        time.sleep(0.5)
+    余额字典 = {
+        "B101" : "", 
+        "B102" : "", 
+        "B219" : ""
+    }
+    while True:
+        for roomID in rooms:
+            电费余额 = 查电费.获取电费json(roomID)
+            if 余额字典[roomID] != 电费余额:
+                余额字典[roomID] = 电费余额
+                生成rss.生成rss文件(roomID, 电费余额)
+            time.sleep(0.1)
+        time.sleep(1)
