17540470157f0e5f93a4e43a39e5d7ae34388897
长岛冰茶、 <iceclock1037@gmail.com>
2020-12-24 20:52:41 +0800

    Add B309

--- src/主程序.py
+++ src/主程序.py
@@ -3,7 +3,7 @@ import 生成rss
 import time
 
 if __name__ == "__main__":
-    rooms = ["B101", "B102", "B219"]
+    rooms = ["B101", "B102", "B219", "B309"]
     for roomID in rooms:
         电费余额 = 查电费.获取电费json(roomID)
         生成rss.生成rss文件(roomID, 电费余额)
